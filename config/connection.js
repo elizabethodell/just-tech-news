7a1c85cb52b0029e484c8d344113fbba3332ed12
ebo16@vt.edu <ebo16@vt.edu>
2021-11-27 13:33:49 -0500

    module 1

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+//import the sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+//keeps heroku password private
+require('dotenv').config();
+
+//create connection to our database, pass in your MySQL info for username and password
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+  
+  module.exports = sequelize;
+
+  
