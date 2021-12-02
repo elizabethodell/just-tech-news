9ec85d032642aa152a988efa10860aa7237d1bd5
ebo16@vt.edu <ebo16@vt.edu>
2021-12-01 19:06:38 -0500

    created login and logout capabilty

--- server.js
+++ server.js
@@ -15,6 +15,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 // turn on routes
 app.use(routes);
 
